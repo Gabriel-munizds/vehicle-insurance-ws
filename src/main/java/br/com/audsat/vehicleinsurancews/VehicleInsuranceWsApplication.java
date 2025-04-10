00faffc895791b7940853188c7427d98fafb4fd0
João Gabriel Muniz da Silva <gabrielmunizdasilva40@gmail.com>
2025-04-09 21:39:16 -0300

    configuracao inicial projeto spring

---
+++ src/main/java/br/com/audsat/vehicleinsurancews/VehicleInsuranceWsApplication.java
@@ -0,0 +1,13 @@
+package br.com.audsat.vehicleinsurancews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleInsuranceWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleInsuranceWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/audsat/vehicleinsurancews/VehicleInsuranceWsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.audsat.vehicleinsurancews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleInsuranceWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
